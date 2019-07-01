b817642d715dd09dd389ad51e7933b8be00f17cc
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-07-01 12:56:27 +0530

    Python-XII: Update MySQL-python code
    * addRow() is now a generic function that can be used with any table, 
    not just the student table
    * Add a basic docstring for functions
    
    TODO: Generic, dynamic createTable() function

--- MySQL_Connection.py
+++ MySQL_Connection.py
@@ -2,40 +2,53 @@
     PYTHON-MYSQL CONNECTION
 """
 
-import mysql.connector as sql
+import sys, mysql.connector as sql
 
+# Initialize connection to local database 'hackermann'
 conn = sql.connect(host = 'localhost',
                    user = 'root',
                    password = '0021',
                    database = 'hackermann')
 
-if conn.is_connected(): print("Database 'hackermann' connected succesfully\n")
-else: print("Failed to connect to database 'hackermann'!")
+if conn.is_connected():
+    print("DB connected succesfully!")
+    cur = conn.cursor()
+else:
+    print("Connecting to database failed!")
+    sys.exit()
 
-cur = conn.cursor()
-
-def createTable(name):
+def createStudentTable(name):
+    ''' Create pre-defined table named 'student' '''
     cur.execute("create table %s (s_id int primary key, s_name char, marks int)" % name)
     conn.commit()
     print("Table", name, "created succesfully")
 
-def addStudent(t_name):
-    id = int(input("Enter student ID: "))
-    name = input("Enter student name: ")
-    marks = int(input("Enter marks: "))
-    cur.execute("insert into %s values(%d, '%s', %d)" % (t_name, id, name, marks))
+def addRow(t_name):
+    ''' Add a row in any generic table '''
+    cur.execute('desc %s' % t_name)
+    colnames = [i[0] for i in cur.fetchall()]
+    values = [t_name]
+    for col in colnames: values.append(input("Enter '%s' value: " % col))
+
+    cur.execute("insert into %s values(%s, '%s', %s)" % tuple(values))
     conn.commit()
-    print("Student added succesfully!")
+    print("Row added succesfully!")
+
+def display(data):
+    ''' Display given fetchall() data '''
+    colnames = [i[0] for i in cur.description]
+    for i in data:
+        print()
+        for j in range(len(i)):
+            print(colnames[j], "value:", i[j])
 
 def displayTable(name):
+    ''' Display entire contents of a specified table '''
     cur.execute("select * from %s" % name)
     colnames = [i[0] for i in cur.description]
     print("Table", name, "contents:")
-    for i in cur.fetchall():
-        print()
-        for j in range(len(i)):
-            print(colnames[j], "=", i[j])
+    display(cur.fetchall())
 
-#addStudent("student")
-#createTable("haha")
+#createStudentTable("haha")
+#addRow("student")
 displayTable("student")
